05fd72dedfbb5b2237915787bd55491973e80d6e
carlos0550 <carlospelinski03@gmail.com>
2025-04-21 01:21:20 -0300

    feat: Implementa mejor forma de acceder a apis entre entornos

--- frontend/src/GlobalAPIs.ts
+++ frontend/src/GlobalAPIs.ts
@@ -1,7 +1,32 @@
-const base_url = "http://localhost:"
+const isProduction = true
+console.log(isProduction)
+const BASE_URL_DEV = "http://localhost"
 
-const services_dns = {
-    "authentication": "5000"
+// Aquí definís todos tus servicios
+type ServiceConfig = {
+  port: string
+  path: string
+  prodUrl?: string // opcional, por si ese microservicio está en un dominio aparte
 }
 
-export const authentication_url = new URL(base_url + services_dns.authentication + "/api/")
+const services: Record<string, ServiceConfig> = {
+  authentication: {
+    port: "5000",
+    path: "/api/",
+    prodUrl: "https://e-commerce-development.up.railway.app", 
+  },
+  
+}
+
+export const getServiceUrl = (service: keyof typeof services): string => {
+    const { port, path, prodUrl } = services[service]
+  
+    if (isProduction) {
+      return new URL(path, prodUrl).toString()
+    } else {
+      const url = new URL(BASE_URL_DEV)
+      url.port = port
+      url.pathname = path
+      return url.toString()
+    }
+  }
