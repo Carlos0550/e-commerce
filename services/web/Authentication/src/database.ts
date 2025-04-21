71601481cac755022095dcd468ef0b05b21e63d3
carlos0550 <carlospelinski03@gmail.com>
2025-04-21 01:01:43 -0300

    feat: agregado conn string de base de bd

--- services/web/Authentication/src/database.ts
+++ services/web/Authentication/src/database.ts
@@ -1,11 +1,18 @@
 import { Pool } from "pg"
 
+// const pool = new Pool({
+//     user: "postgres",
+//     host: "localhost",
+//     database: "e-commerce",
+//     password: "35218889",
+//     port: 5432,
+// })
+
 const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "e-commerce",
-    password: "35218889",
-    port: 5432,
+    connectionString: process.env.RAILWAY_PG_CONNECTION_STRING,
+    ssl: {
+        rejectUnauthorized: false
+    }
 })
 
 export default pool
