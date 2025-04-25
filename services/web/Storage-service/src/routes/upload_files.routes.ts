14d26693438f7e08da584d97d8a1769de5582390
carlos0550 <carlospelinski03@gmail.com>
2025-04-25 11:46:32 -0300

    feat: router de productos

---
+++ services/web/Storage-service/src/routes/upload_files.routes.ts
@@ -0,0 +1,63 @@
+import { Router, Request, Response, NextFunction } from "express";
+import uploads from "../utils/SaveFilesFromRequest";
+import { ProductFormValues } from "../Types/UploadFilesTypes";
+import { checkIsAdmin } from "../utils/CheckIsAdmin";
+import { rollbackFiles } from "../utils/RollbackFiles";
+import { QueryWithUserId } from "../Types/QueryWithUserId";
+import { saveProduct } from "../controllers/upload_files.controller";
+
+const router = Router();
+
+router.post("/create-product", uploads.array("product_images"), async (
+    req: Request<{}, {}, ProductFormValues, QueryWithUserId>,
+    res: Response,
+    next: NextFunction
+): Promise<void> => {
+    const files = req.files as Express.Multer.File[];
+    const { user_id } = req.query
+    if (!files || files.length === 0) {
+        res.status(400).json({ msg: "No se han proporcionado archivos" });
+        return;
+    }
+
+    try {
+        if (!user_id) {
+            throw new Error("Usuario no autorizado (falta user_id)");
+        }
+
+        const is_admin = await checkIsAdmin(user_id);
+        if (!is_admin) {
+            throw new Error("Usuario no autorizado (no es admin)");
+        }
+
+        const {
+            product_name,
+            product_description,
+            product_price,
+            product_stock,
+            product_category,
+        } = req.body;
+
+        if (
+            !product_name ||
+            !product_description ||
+            !product_price ||
+            !product_stock ||
+            !product_category
+        ) {
+            throw new Error("Todos los campos son requeridos");
+        }
+
+        console.log(files)
+
+        next();
+    } catch (error: any) {
+        console.error("Error en /create-product:", error.message);
+        await rollbackFiles(files.map((file) => file.filename));
+        res.status(400).json({
+            msg: error.message || "Error inesperado al crear el producto",
+        });
+    }
+}, saveProduct);
+
+export default router;
