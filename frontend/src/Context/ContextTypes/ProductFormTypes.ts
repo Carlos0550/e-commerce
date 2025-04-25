04deee894a9b04e898548e86be6fa4ad392a828c
carlos0550 <carlospelinski03@gmail.com>
2025-04-25 11:37:32 -0300

    feat: tipado de productos

---
+++ frontend/src/Context/ContextTypes/ProductFormTypes.ts
@@ -0,0 +1,14 @@
+export interface ProductImages{
+    image_name: string,
+    originFileObj: File,
+    image_id: string,
+    isNew: boolean
+}
+export interface ProductFormValues{
+    product_name: string,
+    product_description: string,
+    product_price: string,
+    product_stock: string,
+    product_category: string,
+    product_images: ProductImages[],
+}
