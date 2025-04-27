4fededb88e30629ce88a4fc902ceb9bde02d443d
carlos0550 <carlospelinski03@gmail.com>
2025-04-27 11:49:50 -0300

    Feat: agrega editor de Mantine

--- frontend/src/Sections/Manager/ProductManager/utils/useProductForm.tsx
+++ frontend/src/Sections/Manager/ProductManager/utils/useProductForm.tsx
@@ -3,9 +3,11 @@ import { ProductFormValues } from "../../../../Context/ContextTypes/ProductFormT
 import { v4 as uuid } from "uuid"
 import { showNotification } from "@mantine/notifications";
 import { useAppContext } from "../../../../Context/AppContext";
+import StarterKit from "@tiptap/starter-kit";
+import { useEditor } from "@tiptap/react";
 function useProductForm() {
   const {
-    productsHook:{
+    productsHook: {
       saveProduct
     }
   } = useAppContext()
@@ -18,6 +20,11 @@ function useProductForm() {
     product_stock: "",
   })
 
+  const editor = useEditor({
+    extensions: [StarterKit],
+    content: productForm.product_description,
+  });
+
   const removeImage = (image_id: string) => {
     setProductForm((prev) => ({
       ...prev,
@@ -25,23 +32,23 @@ function useProductForm() {
     }));
   };
   const handleUploadImages = (files: File[]) => {
-    if(productForm.product_images.length + files.length > 6) {
+    if (productForm.product_images.length + files.length > 6) {
       showNotification({
         color: "red",
         title: "Error",
         message: "No puedes subir mas de 5 imagenes",
         position: "top-right",
-        autoClose:2500
+        autoClose: 2500
       })
       return
     }
-    if(productForm.product_images.length === 6){
+    if (productForm.product_images.length === 6) {
       showNotification({
         color: "red",
         title: "Error",
         message: "No puedes subir mas de 5 imagenes",
         position: "top-right",
-        autoClose:2500
+        autoClose: 2500
       })
       return
     }
@@ -52,7 +59,7 @@ function useProductForm() {
         type: file.type,
         lastModified: file.lastModified,
       });
-  
+
       return {
         image_name: `${date}-${uuid().slice(0, 8)}`,
         originFileObj: renamedFile,
@@ -60,15 +67,15 @@ function useProductForm() {
         isNew: true,
       };
     });
-  
+
     setProductForm((prev) => ({
       ...prev,
       product_images: [...prev.product_images, ...newImages],
     }));
   };
-  
 
-  const handleChangeValues = (e:React.ChangeEvent<HTMLInputElement>) => {
+
+  const handleChangeValues = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
     setProductForm((prevValues) => ({
       ...prevValues,
@@ -95,80 +102,97 @@ function useProductForm() {
     } = productForm
 
     for (const key in productForm) {
+      if (key === "product_images" || key === "product_category") continue
       if (productForm[key as keyof ProductFormValues] === "") {
         showNotification({
           color: "red",
           title: "Error",
           message: `El campo "${traducciones[key as keyof ProductFormValues]}" es obligatorio`,
           position: "top-right",
-          autoClose:2500
+          autoClose: 2500
         })
         return false
       }
     }
 
-    if(product_images.length === 0){
+    if (product_images.length === 0) {
       showNotification({
         color: "red",
         title: "Error",
         message: "Debes subir al menos una imagen",
         position: "top-right",
-        autoClose:2500
+        autoClose: 2500
       })
       return false
     }
 
-    if(product_name.length < 6){
+    if (product_name.length < 6) {
       showNotification({
         color: "red",
         title: "Error",
         message: "El nombre debe tener al menos 6 caracteres",
         position: "top-right",
-        autoClose:2500
+        autoClose: 2500
       });
       return false
     }
 
-    if(product_description === "<p></p>" || product_description.length < 20){
+    if (product_description === "<p></p>" || product_description.length < 20) {
       showNotification({
         color: "red",
         title: "Error",
         message: "La descripcion debe tener al menos 20 caracteres",
         position: "top-right",
-        autoClose:2500
+        autoClose: 2500
       });
       return false
     }
 
-    if(parseFloat(product_stock) <= 0){
+    if (parseFloat(product_stock) <= 0) {
       showNotification({
         color: "red",
         title: "Error",
         message: "El stock debe ser mayor a 0",
         position: "top-right",
-        autoClose:2500
+        autoClose: 2500
       });
       return false
     }
 
-    if(parseFloat(product_price) <= 0){
+    if (parseFloat(product_price) <= 0) {
       showNotification({
         color: "red",
         title: "Error",
         message: "El precio debe ser mayor a 0",
         position: "top-right",
-        autoClose:2500
+        autoClose: 2500
       });
       return false
     }
     return true
   }
 
+  const handleClearFields = (): void => {
+    setProductForm({
+      product_name: "",
+      product_description: "",
+      product_price: "",
+      product_stock: "",
+      product_category: "",
+      product_images: []
+    })
+    editor?.commands.clearContent();
+    return;
+  }
 
-  const onFinish = (e:React.FormEvent) => {
+  const [savingProduct, setSavingProduct] = useState(false)
+  const onFinish = async (e: React.FormEvent): Promise<void> => {
     e.preventDefault()
-    if(validateForm()){
-      saveProduct(productForm)
+    if (validateForm()) {
+      setSavingProduct(true)
+      const result = await saveProduct(productForm)
+      setTimeout(() => setSavingProduct(false), 1000)
+      if (result) handleClearFields()
     }
   }
 
@@ -177,7 +201,9 @@ function useProductForm() {
     productForm,
     handleChangeValues,
     removeImage,
-    onFinish
+    onFinish,
+    savingProduct,
+    editor
   }
 }
 
