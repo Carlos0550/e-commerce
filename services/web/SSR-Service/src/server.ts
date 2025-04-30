ba2cecdf8d286737efa4b5f0261ffc471dee782d
carlos0550 <carlospelinski03@gmail.com>
2025-04-29 22:57:22 -0300

    feat: puerto de SSR cambiado para desarrollo

--- services/web/SSR-Service/src/server.ts
+++ services/web/SSR-Service/src/server.ts
@@ -25,7 +25,7 @@ app.get("/", (_req: Request, res: Response) => {
 });
 
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5002;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
