c9ab29afbaf9f8c0e9518cee4946ffd1309abaea
carlos0550 <carlospelinski03@gmail.com>
2025-05-03 00:40:58 -0300

    Chore: Cambios menores

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,13 +4,15 @@ import { Routes, Route} from "react-router-dom"
 import LoginManager from './Sections/LoginSection/LoginManager'
 import ManagerHome from './Sections/Manager/ManagerHome'
 import HomePage from './Sections/HomePage/HomePage'
+import HomeProductView from './Sections/HomePage/components/Home/components/HomeProductView/HomeProductView'
 
 function App() {
   
   
   return (
     <Routes>
-      <Route path='/*' element={<HomePage/>}/>
+      <Route path='/' element={<HomePage/>}/>
+      <Route path="/product-details/:product_id" element={<HomeProductView/>}/>
       <Route path='/authentication' element={<LoginManager/>}/>
       <Route path='/admin-dashboard/*' element={<ManagerHome/>}/>
     </Routes>
--- frontend/src/Sections/HomePage/HomePage.tsx
+++ frontend/src/Sections/HomePage/HomePage.tsx
@@ -1,14 +1,9 @@
-import { Route, Routes } from "react-router-dom"
 import "./HomePage.css"
 import Home from "./components/Home/Home"
-import HomeProductView from "./components/Home/components/HomeProductView/HomeProductView"
 function HomePage() {
   return (
     <div className="home-page-wrapper">
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/product-details/:product_id" element={<HomeProductView/>}/>
-      </Routes>
+      <Home/>
     </div>
   )
 }
