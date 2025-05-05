abbb3915fb95bf098a9494fadbe63cbff25b5093
carlos0550 <carlospelinski03@gmail.com>
2025-05-05 14:40:22 -0300

    feat: tipado de carrito

---
+++ frontend/src/Context/ContextTypes/CartTypes.tsx
@@ -0,0 +1,9 @@
+
+export interface Cart{
+    product_id: string,
+    product_name: string,
+    product_price: string,
+    product_stock: string,
+    product_images: string,
+    product_quantity: number
+}
